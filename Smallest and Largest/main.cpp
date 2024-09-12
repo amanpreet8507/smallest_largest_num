e3f1d86d7e11ef71d583139b19a93108b08f60ea
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-09-12 00:05:50 -0700

    Initial Commit

---
+++ Smallest and Largest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Smallest and Largest
+//
+//  Created by Amanpreeet Sandhu on 2024-09-12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
